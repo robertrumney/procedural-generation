9bd12207548b29b0a6b653de54307dc028e762ff
Robert Rumney <robertrumney@gmail.com>
2023-04-16 14:04:42 +0200

    Update ProceduralMachine.cs

--- ProceduralMachine.cs
+++ ProceduralMachine.cs
@@ -1,36 +1,35 @@
 using UnityEngine;
-
 using UnityEngine.UI;
 using UnityEngine.AI;
-
 using System.Collections;
 using System.Collections.Generic;
 
 public class ProceduralMachine : MonoBehaviour
 {
-    #region Configuration
+    // Singleton instance
     public static ProceduralMachine instance;
 
-    public bool disabled=false;
-    public int range=20;
-    public int amount=10;
-    public int minThreshold=40;
-    public int minNavmeshSafeDistance=100;
-
+    // Configurable parameters
+    public bool disabled = false;
+    public int range = 20;
+    public int amount = 10;
+    public int minThreshold = 40;
+    public int minNavmeshSafeDistance = 100;
     public Slider qualitySlider;
     public List<ItemDefinition> spawn;
-    public List<OptimizationVolume> volumes = new List<OptimizationVolume>(); 
+    public List<OptimizationVolume> volumes = new List<OptimizationVolume>();
 
+    // Private variables
     private RaycastHit hit;
     private Vector3 prevPoint;
     private OptimizationVolume prevOv;
-    
     private float lastValue;
-    private bool listeningToValueChange=false;
+    private bool listeningToValueChange = false;
 
+    // Item definition class
     [System.Serializable]
-	  public class ItemDefinition
-	  {
+    public class ItemDefinition
+    {
         public string name;
         public GameObject prefab;
         public Vector3 positionOffset;
@@ -38,96 +37,78 @@ public class ProceduralMachine : MonoBehaviour
         public float odds;
     }
 
+    // Called when the object is initialized
     private void Awake()
     {
-        instance=this;
+        instance = this;
     }
 
-    private void BeginValueChangeListen()
-    {
-        this.gameObject.SetActive(true);
-        StartCoroutine(ListenToValueChange());
-    }
-
-    private IEnumerator ListenToValueChange()
-    {
-        float time=0;
-
-        while(true)
-        {
-            time+=Time.deltaTime;
-
-            if(time > 2)
-            {
-                if(qualitySlider.value!=lastValue)
-                {   
-                    RepeatSpawn(qualitySlider.value);
-                    listeningToValueChange=false;
-                    yield break;
-                }
-            }
-
-            yield return null;
-        }
-    }
-
+    // Set the active state of all spawned objects
     public void SetActiveAllObjects(bool x)
     {
-        foreach(OptimizationVolume volume in volumes)
+        foreach (OptimizationVolume volume in volumes)
         {
-            foreach(GameObject go in volume.spawnedItems)
+            foreach (GameObject go in volume.spawnedItems)
             {
-                if(go!=null)
+                if (go != null)
                     go.SetActive(x);
             }
         }
     }
 
-    
+    // Spawn objects in the specified optimization volume
     public void Do(OptimizationVolume ov)
     {
-        if(disabled) return;
+        // If the machine is disabled or the volume cannot spawn more objects, return
+        if (disabled) return;
+        if (ov.CannotSpawnMoreItems(this)) return;
 
-        if(ov.CannotSpawnMoreItems(this)) return;
-            
-        Vector3 originPoint = this.transform.position + new Vector3(0,100,0);
+        // Calculate the origin point for spawning objects
+        Vector3 originPoint = this.transform.position + new Vector3(0, 100, 0);
 
-        for(int i=0;i<amount;i++)
+        // Spawn the specified amount of objects
+        for (int i = 0; i < amount; i++)
         {
-            int rX = Random.Range(-range,range);
-            int rY = Random.Range(-range,range);
+            // Randomize the spawn position within the specified range
+            int rX = Random.Range(-range, range);
+            int rY = Random.Range(-range, range);
+            Vector3 _originPoint = originPoint + new Vector3(rX, 0, rY);
 
-            Vector3 _originPoint = originPoint + new Vector3(rX,0,rY);
-
-            if (Physics.Raycast(_originPoint, -transform.up, out hit, 500)) 
+            // Check if the spawn position is valid
+            if (Physics.Raycast(_originPoint, -transform.up, out hit, 500))
             {
-                if (hit.collider.gameObject.CompareTag("Ground") && hit.collider.name !="Terrain START")
+                // Check if the hit object is the ground and not the starting terrain
+                if (hit.collider.gameObject.CompareTag("Ground") && hit.collider.name != "Terrain START")
                 {
                     Vector3 targetPos = hit.point;
 
+                    // Check if the spawn point is on the NavMesh and within the minimum safe distance
                     NavMeshHit _hit;
 
-                    if(!NavMesh.SamplePosition(targetPos, out _hit, 1f, NavMesh.AllAreas))
+                    if (!NavMesh.SamplePosition(targetPos, out _hit, 1f, NavMesh.AllAreas))
                     {
-                        float distance=Vector3.Distance(targetPos, this.transform.position);
-                        
-                        if(distance > minThreshold)
+                        float distance = Vector3.Distance(targetPos, this.transform.position);
+
+                        if (distance > minThreshold)
                         {
-                            int x = Random.Range(0,spawn.Count);   
-                            float r = Random.Range(0.0f,1.0f);
+                            int x = Random.Range(0, spawn.Count);
+                            float r = Random.Range(0.0f, 1.0f);
 
-                            if(spawn[x].prefab==null) continue;
+                            // Check if the selected object is valid and the spawn chance is met
+                            if (spawn[x].prefab == null) continue;
 
-                            if(r<=spawn[x].odds)
+                            if (r <= spawn[x].odds)
                             {
-                                float rot = Random.Range(0,360);
+                                float rot = Random.Range(0, 360);
 
-                                GameObject go = Instantiate(spawn[x].prefab,targetPos,Quaternion.identity) as GameObject;
+                                // Instantiate the object and apply rotation and position offsets
+                                GameObject go = Instantiate(spawn[x].prefab, targetPos, Quaternion.identity) as GameObject;
                                 Quaternion targetRotation = Quaternion.FromToRotation(go.transform.up, hit.normal) * go.transform.rotation;
                                 go.transform.rotation = targetRotation;
-                                go.transform.localEulerAngles = new Vector3(go.transform.localEulerAngles.x,rot,go.transform.localEulerAngles.z);
+                                go.transform.localEulerAngles = new Vector3(go.transform.localEulerAngles.x, rot, go.transform.localEulerAngles.z);
 
-                                ov.SpawnItem(go);             
+                                // Add the object to the optimization volume
+                                ov.SpawnItem(go);
                             }
                         }
                     }
@@ -136,59 +117,47 @@ public class ProceduralMachine : MonoBehaviour
         }
     }
 
-    public void ValueChanged(float x)
+    // Spawn objects at a specified point within an optimization volume
+    public void SpawnAtPoint(OptimizationVolume ov, Vector3 originPoint)
     {
-        if(!listeningToValueChange)
+        // Store the point and volume for repeat spawning
+        prevPoint = originPoint;
+        prevOv = ov;
+
+        // Spawn the specified amount of objects
+        for (int i = 0; i < amount; i++)
         {
-            lastValue=x;
-            BeginValueChangeListen();
-            listeningToValueChange=true;
-        }
-    }
+            // Randomize the spawn position within the specified range
+            int rX = Random.Range(-range, range);
+            int rY = Random.Range(-range, range);
+            Vector3 _originPoint = originPoint + new Vector3(rX, 0, rY);
 
-    public void RepeatSpawn(float x)
-    {
-        if(disabled)
-            return;
-
-        prevOv.ForceClear();
-        amount=Mathf.RoundToInt(x);
-        SpawnAtPoint(prevOv,prevPoint);
-    }
-
-    public void SpawnAtPoint(OptimizationVolume ov,Vector3 originPoint)
-    {
-        prevPoint=originPoint;
-        prevOv=ov;
-
-        for(int i=0;i<amount;i++)
-        {
-            int rX = Random.Range(-range,range);
-            int rY = Random.Range(-range,range);
-            Vector3 _originPoint = originPoint + new Vector3(rX,0,rY);
-
-            if (Physics.Raycast(_originPoint, -transform.up, out hit, 500)) 
+            // Check if the spawn position is valid
+            if (Physics.Raycast(_originPoint, -transform.up, out hit, 500))
             {
-                if (hit.collider.gameObject.layer==10)
+                // Check if the hit object is on a specific layer
+                if (hit.collider.gameObject.layer == 10)
                 {
                     Vector3 targetPos = hit.point;
-                
-                    int x = Random.Range(0,spawn.Count);   
-                    float r = Random.Range(0.0f,1.0f);
 
-                    float distance=Vector3.Distance(targetPos,Camera.main.transform.position);
+                    int x = Random.Range(0, spawn.Count);
+                    float r = Random.Range(0.0f, 1.0f);
 
-                    if(distance > minThreshold)
+                    float distance = Vector3.Distance(targetPos, Camera.main.transform.position);
+
+                    // Check if the selected object is valid and the spawn chance is met
+                    if (distance > minThreshold)
                     {
-                        if(r<=spawn[x].odds)
+                        if (r <= spawn[x].odds)
                         {
-                            float rot = Random.Range(0,360);
+                            float rot = Random.Range(0, 360);
 
-                            GameObject go = Instantiate(spawn[x].prefab,targetPos,Quaternion.identity) as GameObject;
+                            // Instantiate the object and apply rotation and position offsets
+                            GameObject go = Instantiate(spawn[x].prefab, targetPos, Quaternion.identity) as GameObject;
                             Quaternion targetRotation = Quaternion.FromToRotation(go.transform.up, hit.normal) * go.transform.rotation;
                             go.transform.rotation = targetRotation;
-                            go.transform.localEulerAngles = new Vector3(go.transform.localEulerAngles.x,rot,go.transform.localEulerAngles.z);
-                            ov.SpawnItem(go);             
+                            go.transform.localEulerAngles = new Vector3(go.transform.localEulerAngles.x, rot, go.transform.localEulerAngles.z);
+                            ov.SpawnItem(go);
                         }
                     }
                 }
